849dd55d6b718839c3579041b56d946579ada682
Wallace144 <willfs144@gmail.com>
2016-06-30 22:58:13 -0500

    logout o cerrar sesión quedo funcionando

--- app/src/main/java/co/distrital/ud/twitterclient/LoginActivity.java
+++ app/src/main/java/co/distrital/ud/twitterclient/LoginActivity.java
@@ -9,15 +9,13 @@ import android.widget.RelativeLayout;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import co.distrital.ud.twitterclient.main.MainActivity;
-import io.fabric.sdk.android.Fabric;
+import co.distrital.ud.twitterclient.main.ui.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -65,7 +63,11 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void navigateToMainScreen() {
-        startActivity(new Intent(this, MainActivity.class));
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 
